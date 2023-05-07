ace4df570b9242eea33213158471f5634a1b180b
Umer Arshad <umerm64@gmail.com>
2023-05-08 02:53:37 +0500

    client app files

---
+++ client.py
@@ -0,0 +1,25 @@
+import requests as req
+import time
+import hashlib
+
+if __name__ == '__main__':
+    try:
+        filename = '/clientdata/file.txt'
+        res = req.get('http://server:5000/download_file')
+        if (res.status_code == 200):
+            f = open(filename, 'wb')
+            f.write(res.content)
+            f.close()
+
+            res = req.get('http://server:5000/checksum')
+            if (res.status_code == 200) and (res.text == hashlib.md5(open(filename,'rb').read()).hexdigest()):
+                print('Correct file downloaded!')
+            else:
+                print('Integrity failed!')
+        else:
+            print('Error getting file from server,', res.status_code)
+    except Exception as e:
+        print('Exception: ', e)
+
+    # while(True):
+    #     time.sleep(1)
